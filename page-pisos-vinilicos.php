75795ce2129a04ed1200939e0a94bf0a4e740fc7
Rafael Brunner <rafael_nbc@hotmail.com>
2023-03-10 17:31:56 -0300

    update

--- page-pisos-vinilicos.php
+++ page-pisos-vinilicos.php
@@ -46,6 +46,32 @@
 						<?php endwhile; endif; wp_reset_query()?>
 					</div>
 			</div>
+			<div class="ospefloor mt-5">
+				<header class="product-title d-flex ai-center my-5">
+						<h2>Pisos Vinilicos Durafloor</h2>
+						<img class="ml-2" width=80 height=80 src="<?php the_field('page_provider_ospefloor'); ?>" alt="">
+				</header>
+					<?php 
+						$config = array ('posts_per_page' => '6', 'post_type' => 'pisos-vinilicos', 'category_name' => 'ospefloor', 'order'=> 'DESC');
+						$query_posts = new WP_Query($config);
+					?>
+					<div class="grid mb-10">
+						<?php if(have_posts()):while($query_posts -> have_posts()):$query_posts -> the_post();?>
+								<div class="card-produto mb-5">
+									<div class="card-img mb-4">	
+										<a href="<?php the_permalink()?>">
+											<img src="<?php the_field('single_product_thumbmail'); ?>" 
+											alt="<?php the_field('single_alt_text_thumbmail'); ?>">
+										</a>
+									</div>
+									<div class="card-content d-flex jc-between ai-center">
+										<h3><?php the_title()?></h3>
+										<a class="d-flex jc-between ai-center" href="<?php the_permalink()?>">Saiba mais <img  class="icon" src="<?php echo get_template_directory_uri()?>/img/icon-saiba-mais.svg" alt=""></a>
+									</div>
+								</div>
+						<?php endwhile; endif; wp_reset_query()?>
+					</div>
+			</div>
 		</div>
 	</section>
 </main>
