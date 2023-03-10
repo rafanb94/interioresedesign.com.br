24ce850ad0b15db8274ce340436bf3eeb7020ff1
Rafael Brunner <rafael_nbc@hotmail.com>
2023-03-10 19:29:19 -0300

    update

--- single-pisos-vinilicos.php
+++ single-pisos-vinilicos.php
@@ -22,10 +22,10 @@
 					<?php the_content()?>
 				</div>
 				<div class="row flex-col mt-5">
-					<header>
+					<header class="mb-3">
 						<h2>Fornecedor</h2>
 					</header>
-						<img style="object-fit:contain;" width=180 height=180 src="<?php the_field('single_provider'); ?>" alt="">
+						<img style="object-fit:contain;" src="<?php the_field('single_provider'); ?>" alt="">
 				</div>
 				<div class="product-colors mt-5">
 					<header>
